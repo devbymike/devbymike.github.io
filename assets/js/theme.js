e95a17d21a3be6ec5a03c5ee96dd7a699ffb877b
devbymike <mike@mk3y.com>
2014-12-03 20:08:29 -0500

    major revisions

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -726,60 +726,8 @@ jQuery(document).ready(function(){
 
 	// FITVIDS
 	jQuery(".postbody").fitVids();
-  jQuery(".excerpt").fitVids();
+    jQuery(".excerpt").fitVids();
 	
-
-	// COMMENTS
-	if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined || config.google_comments == true){
-		$('.comments').show();
-		if(config.autoload_comments == true){
-			loadComments();
-			$('.readmore').fadeOut(400);
-		}
-	}
-
-	function loadComments(){
-
-		if(config.disqus_shortname != ''){
-			var disqus_shortname = config.disqus_shortname;
-			(function() {
-			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-			})();
-		}else if(config.google_comments == true){
-			
-			$.getScript("https://apis.google.com/js/plusone.js")
-			.done(function(script, textStatus ) {
-				gapi.comments.render('g-comments', {
-				    href: window.location,
-				    width: '760',
-				    first_party_property: 'BLOGGER',
-				    view_type: 'FILTERED_POSTMOD'
-				});
-			});
-
-		}
-
-	}
-
-	$('.readmore').click(function(){
-		loadComments();
-		$(this).fadeOut(400);
-	});
-
-
-	// ANALYTICS
-	if(config.analytics_id != '' || config.analytics_id != null || config.analytics_id != undefined){
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', config.analytics_id, config.analytics_domain);
-		ga('send', 'pageview');
-	}
-
-
 	// READING TIME
 	if(config.readingtime == true){
 		$(".postbody").readingTime();
